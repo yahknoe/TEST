485c08493ba5014603b4514c7d480bdf309d8bac
Vincent Pham <yahknoe@gmail.com>
2013-02-27 21:53:52 -0800

    what will this do?

---
+++ Foothill.java
@@ -0,0 +1,20 @@
+/** Vincent Pham
+ * 
+ * Prints the message "Hello, World!" to the console
+ *
+ */
+public class Foothill 
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hello, World!"); //prints "Hello, World!" to console
+	}
+}
+
+//BLAHDSKGAHJSLDFGLA
+
+/**Console output:
+ * 
+ * Hello, World!
+ * 
+ */
